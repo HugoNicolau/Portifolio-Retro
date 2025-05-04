50bd925b2a72f6a952bd238fc42735acc3877c3e
HugoNicolau <nicolau.hugogiles@gmail.com>
2025-05-03 23:57:45 -0300

    feat: add tailwindcss-animate for enhanced animations and update configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-import type { Config } from "tailwindcss"
-
+import { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
@@ -75,7 +75,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [animate],
+} satisfies Config;
 
-export default config
+export default config;
