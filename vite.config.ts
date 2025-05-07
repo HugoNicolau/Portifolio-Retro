a5f6b571494fefa844078ef7bb737eea1c6cb047
HugoNicolau <nicolau.hugogiles@gmail.com>
2025-05-07 17:28:54 -0300

    refactor: streamline build process in package.json and update vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { fileURLToPath } from 'url'
 
-// https://vite.dev/config/
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
